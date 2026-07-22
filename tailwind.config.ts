75c050e9cf11d96cb9b0984e6072890562eae075
Sudipto <sudiptobucse11@gmail.com>
2026-07-22 08:41:16 +0600

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#166534",
+        secondary: "#15803d",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
